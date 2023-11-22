0e2d3f8722e5636c2250963158a53cfefae02089
LAPTOP-Q8PH4MAI\PC <solivervmazo.jobs@gmail.com>
2023-11-23 05:24:35 +0800

    New: AppColorPicker && Update: ItemDetail

--- src/ui/AppColorPicker.jsx
+++ src/ui/AppColorPicker.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import ColorPicker, { Panel5 } from "reanimated-color-picker";
-import { appColors, appSpacing } from "../themes";
+import { appColors, appSizes, appSpacing } from "../themes";
 import AppCard from "./AppCard";
+import Spacer from "./core/Spacer";
+import AppModal from "./AppModal";
 
 const AppColorPicker = ({
   colorValue,
@@ -12,23 +14,46 @@ const AppColorPicker = ({
   const _onSelectHandle = ({ hex }) => {
     setColorValue(hex);
     onSelect({ colorValue: hex });
-    console.log(hex);
   };
   return (
-    <View style={{ padding: appSpacing.screenPaddingLeft }}>
-      <ColorPicker value={_colorValue} onComplete={_onSelectHandle}>
-        <Panel5 />
-      </ColorPicker>
-      <AppCard
-        title={"hello"}
-        containerStyle={{
-          flex: 0,
-        }}
-        renderTitle={({ title, titleStyle }) => (
-          <Text style={[titleStyle]}>Hello</Text>
-        )}
-      />
-    </View>
+    <AppModal
+      renderContent={() => (
+        <View style={{ padding: appSpacing.screenPaddingLeft }}>
+          <ColorPicker value={_colorValue} onComplete={_onSelectHandle}>
+            <Panel5 />
+          </ColorPicker>
+          <Spacer size={20} horizontal={false} />
+          <AppCard
+            title={_colorValue || "Select color"}
+            containerStyle={{
+              flex: 0,
+            }}
+            renderTitle={({ title, titleStyle }) => (
+              <View
+                style={{
+                  flexDirection: "row",
+                  gap: 10,
+                  alignItems: "center",
+                }}
+              >
+                <View
+                  style={{
+                    height: appSizes.Icon.xLarge,
+                    width: appSizes.Icon.xLarge,
+                    backgroundColor: _colorValue,
+                    borderWidth: 2,
+                    borderBlockColor: appColors.themeColor,
+                    borderRadius: appSizes.Icon.xLarge / 4,
+                    overflow: "hidden",
+                  }}
+                />
+                <Text style={[titleStyle]}>{title}</Text>
+              </View>
+            )}
+          />
+        </View>
+      )}
+    />
   );
 };
 
